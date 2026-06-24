bf767f023b3c499bec3bc22f6d6096871c292b4c
Volodymyr <aleksandrov.volodymyr97@gmail.com>
2026-06-24 14:45:30 +0300

    Update page after Delete Products Function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += /* html */ `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -94,12 +94,16 @@ cart.forEach((cartItem) => {
     `
 });
 
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
 
-document.querySelector('.js-delete-link')
+document.querySelectorAll('.js-delete-link')
     .forEach((link) => {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
     });
