82e047c52770524ee4198a2e92412ec2516d39f5
Volodymyr <aleksandrov.volodymyr97@gmail.com>
2026-09-08 11:03:41 +0300

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -69,6 +69,8 @@ export function loadProductsFetch() {
       });
 
       console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -94,6 +96,10 @@ export function loadProducts(fun) {
       console.log('load products');
 
       fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
